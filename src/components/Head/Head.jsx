c08fa1cb1fa4b9851d903e51cfa68369bd4519cb
gehrig rankin <rgrankin22@gmail.com>
2018-10-05 11:54:04 -0700

    added header

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -6,6 +6,16 @@ const Head = (props) => {
     return (
         <div className="Head">
             <h1 className="title">{props.month} {props.year}</h1>
+
+            <div className="daysOfWeek columns">
+                <div className="column">Sunday</div>
+                <div className="column">Monday</div>
+                <div className="column">Tuesday</div>
+                <div className="column">Wednesday</div>
+                <div className="column">Thursday</div>
+                <div className="column">Friday</div>
+                <div className="column">Saturday</div>
+            </div>
         </div>
     )
 }
